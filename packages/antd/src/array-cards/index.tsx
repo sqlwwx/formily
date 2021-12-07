8eff9620844291ae44fe3a0a82fb7ad5f99afaca
sqlwwx <wwx@wuweixing.com>
2021-12-07 14:34:54 +0800

    feat(antd): array-cards support pagination

--- packages/antd/src/array-cards/index.tsx
+++ packages/antd/src/array-cards/index.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import { Card, Empty } from 'antd'
+import React, { Fragment, useState, useEffect } from 'react'
+import { Card, Empty, Pagination, Space, Select, Badge } from 'antd'
 import { CardProps } from 'antd/lib/card'
 import { IGridOptions } from '@formily/grid'
 import { ArrayField } from '@formily/core'
 import {
+  useForm,
   useField,
   observer,
   useFieldSchema,
   RecursionField,
 } from '@formily/react'
+import { isBool } from '@formily/shared'
 import cls from 'classnames'
 import { ISchema } from '@formily/json-schema'
+import { SelectProps } from 'antd/lib/select'
 import { usePrefixCls } from '../__builtins__'
 import { ArrayBase, ArrayBaseMixins, IArrayBaseProps } from '../array-base'
 import FormGrid from '../form-grid'
 
 const { GridColumn } = FormGrid
 
+interface IStatusSelectProps extends SelectProps<any> {
+  pageSize?: number
+}
+
 interface ArrayCardsProps extends CardProps {
   grid?: IGridOptions
+  pagination?: any
 }
 
 type ComposedArrayCards = React.FC<
@@ -60,17 +68,133 @@ const isOperationComponent = (schema: ISchema) => {
   )
 }
 
+const StatusSelect: React.FC<IStatusSelectProps> = observer((props) => {
+  const form = useForm()
+  const field = useField<ArrayField>()
+  const prefixCls = usePrefixCls('formily-array-table')
+  const errors = form.queryFeedbacks({
+    type: 'error',
+    address: `${field.address}.*`,
+  })
+  const parseIndex = (address: string) => {
+    return Number(
+      address
+        .slice(address.indexOf(field.address.toString()) + 1)
+        .match(/(\d+)/)?.[1]
+    )
+  }
+
+  const options = props.options?.map(({ label, value }) => {
+    const hasError = errors.some(({ address }) => {
+      const currentIndex = parseIndex(address)
+      const startIndex = (value - 1) * props.pageSize
+      const endIndex = value * props.pageSize
+      return currentIndex >= startIndex && currentIndex <= endIndex
+    })
+    return {
+      label: hasError ? <Badge dot>{label}</Badge> : label,
+      value,
+    }
+  })
+
+  const width = String(options?.length).length * 15
+
+  return (
+    <Select
+      value={props.value}
+      onChange={props.onChange}
+      options={options}
+      virtual
+      style={{
+        width: width < 60 ? 60 : width,
+      }}
+      className={cls(`${prefixCls}-status-select`, {
+        'has-error': errors?.length,
+      })}
+    />
+  )
+})
+
+const ArrayCardsPagination = (props) => {
+  const [current, setCurrent] = useState(1)
+  const prefixCls = usePrefixCls('formily-array-cards')
+  const pageSize = props.pageSize || 10
+  const size = props.size || 'default'
+  const dataSource = props.dataSource || []
+  const startIndex = (current - 1) * pageSize
+  const endIndex = startIndex + pageSize - 1
+  const total = dataSource?.length || 0
+  const totalPage = Math.ceil(total / pageSize)
+  const pages = Array.from(new Array(totalPage)).map((_, index) => {
+    const page = index + 1
+    return {
+      label: page,
+      value: page,
+    }
+  })
+  const handleChange = (current: number) => {
+    setCurrent(current)
+  }
+
+  useEffect(() => {
+    if (totalPage > 0 && totalPage < current) {
+      handleChange(totalPage)
+    }
+  }, [totalPage, current])
+
+  const renderPagination = () => {
+    if (totalPage <= 1) return
+    return (
+      <div className={`${prefixCls}-pagination`}>
+        <Space>
+          <StatusSelect
+            value={current}
+            pageSize={pageSize}
+            onChange={handleChange}
+            options={pages}
+            notFoundContent={false}
+          />
+          <Pagination
+            {...props}
+            pageSize={pageSize}
+            current={current}
+            total={dataSource.length}
+            size={size}
+            showSizeChanger={false}
+            onChange={handleChange}
+          />
+        </Space>
+      </div>
+    )
+  }
+
+  return (
+    <Fragment>
+      {props.children?.(
+        dataSource?.slice(startIndex, endIndex + 1),
+        renderPagination()
+      )}
+    </Fragment>
+  )
+}
+
 export const ArrayCards: ComposedArrayCards = observer((props) => {
   const field = useField<ArrayField>()
   const schema = useFieldSchema()
   const dataSource = Array.isArray(field.value) ? field.value : []
+  const pagination = isBool(props.pagination) ? {} : props.pagination
   const prefixCls = usePrefixCls('formily-array-cards', props)
   const { onAdd, onCopy, onRemove, onMoveDown, onMoveUp } = props
 
   if (!schema) throw new Error('can not found schema object')
 
-  const renderItems = () => {
-    return dataSource?.map((item, index) => {
+  const defaultRowKey = (record: any) => {
+    return dataSource.indexOf(record)
+  }
+
+  const renderItems = (dataSource) => {
+    return dataSource?.map((item) => {
+      const index = defaultRowKey(item)
       const items = Array.isArray(schema.items)
         ? schema.items[index] || schema.items[0]
         : schema.items
@@ -159,19 +283,36 @@ export const ArrayCards: ComposedArrayCards = observer((props) => {
   }
 
   return (
-    <ArrayBase
-      onAdd={onAdd}
-      onCopy={onCopy}
-      onRemove={onRemove}
-      onMoveUp={onMoveUp}
-      onMoveDown={onMoveDown}
-    >
-      {renderEmpty()}
-      <FormGrid {...(props.grid || { maxColumns: 1 })}>
-        {renderItems()}
-      </FormGrid>
-      {renderAddition()}
-    </ArrayBase>
+    <Fragment>
+      {!dataSource?.length && (
+        <Card
+          {...props}
+          onChange={() => {}}
+          className={cls(`${prefixCls}-item`, props.className)}
+          title={props.title || field.title}
+        >
+          <Empty />
+        </Card>
+      )}
+      <ArrayCardsPagination {...pagination} dataSource={dataSource}>
+        {(ds, pager) => (
+          <ArrayBase
+            onAdd={onAdd}
+            onCopy={onCopy}
+            onRemove={onRemove}
+            onMoveUp={onMoveUp}
+            onMoveDown={onMoveDown}
+          >
+            {renderEmpty()}
+            <FormGrid {...(props.grid || { maxColumns: 1 })}>
+              {renderItems(ds)}
+            </FormGrid>
+            <div style={{ marginTop: 5, marginBottom: 5 }}>{pager}</div>
+            {renderAddition()}
+          </ArrayBase>
+        )}
+      </ArrayCardsPagination>
+    </Fragment>
   )
 })
 
